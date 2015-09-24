94551d9c54f0ff748f29734861d4e1244102f72a
Tracy Wadleigh <twadleigh@arete.com>
2015-09-23 20:59:56 -0400

    Loading MATLAB.jl is working now.

--- m/Jl.m
+++ m/Jl.m
@@ -1,13 +1,79 @@
 classdef Jl
 
   properties (Constant)
-    call = Jl.init__(@jl_calld);
-    booted = Jl.boot__();
+    
+    % using the debug version?
+    debug = false;
+    
+    % the mex function handle
+    call = Jl.get_handle();
+
+    % julia paths
+    SUFFIX = Jl.get_suffix();
+    JULIA_TOP = 'C:/tw/Julia-0.5.0-dev';
+    JULIA_LIB = [Jl.JULIA_TOP '/lib'];
+    JULIA_BIN = [Jl.JULIA_TOP '/bin'];
+    JULIA_EXE = [Jl.JULIA_BIN '/julia' Jl.SUFFIX '.exe'];
+    JULIA_IMG = [Jl.JULIA_LIB '/julia/sys' Jl.SUFFIX '.dll'];
+
+    % boot file
+    BOOT_FILE_PATH = Jl.get_boot_file_path();
+    
+    % trick to force initialization
+    booted = Jl.boot;    
   end
   
   methods (Static)
-    function v = eval(expr)
-      v = Jl.call('mexeval', expr);
+    
+    function info()
+      Jl.call();
+    end
+    
+    function [v1, v2, v3, v4, v5] = eval(e1, e2, e3, e4, e5)
+      switch nargin
+        case 1
+          v1 = Jl.call('mexeval', e1);
+        case 2
+          [v1, v2] = Jl.call('mexeval', e1, e2);
+        case 3
+          [v1, v2, v3] = Jl.call('mexeval', e1, e2, e3);
+        case 4
+          [v1, v2, v3, v4] = Jl.call('mexeval', e1, e2, e3, e4);
+        case 5
+          [v1, v2, v3, v4, v5] = Jl.call('mexeval', e1, e2, e3, e4, e5);
+      end
+    end
+    
+    function include(fn)
+      Jl.eval(['include("' fn '")']);
+    end
+      
+    function reboot()
+      Jl.raw_include(Jl.BOOT_FILE_PATH);
+    end
+  end
+  
+  methods (Static, Access=private)
+    
+    function hdl = get_handle()
+      if Jl.debug
+        hdl = @jl_calld;
+      else
+        hdl = @jl_call;
+      end
+    end
+    
+    function sfx = get_suffix()
+      if Jl.debug
+        sfx = '-debug';
+      else
+        sfx = '';
+      end
+    end
+
+    function bf = get_boot_file_path()
+      bits = strsplit(mfilename('fullpath'), filesep);
+      bf = strjoin([bits(1:end-1), 'mexboot.jl'], '/');
     end
     
     function raw_eval(expr)
@@ -18,36 +84,17 @@ classdef Jl
       Jl.raw_eval(['include("' fn '")']);
     end
     
-    function raw_include_boot()
-      bits = strsplit(mfilename('fullpath'), filesep);
-      boot_file = strjoin([bits(1:end-1), 'mexboot.jl'], '/');
-      Jl.raw_include(boot_file);
-    end
-  end
-  
-  methods (Static, Access=private)
-    
-    function bl = boot__()
-      Jl.raw_include_boot();
+    function bl = boot()
+      % add julia bin directory to exe path
+      setenv('PATH', [getenv('PATH') pathsep Jl.JULIA_BIN]);
+      
+      % initialize the runtime
+      Jl.call('', Jl.JULIA_LIB, Jl.JULIA_EXE, Jl.JULIA_IMG);
+      
+      % load the boot file
+      Jl.raw_include(Jl.BOOT_FILE_PATH);
+      
       bl = true;
-    end
-    
-    function hdl = init__(hdl)
-      % for some reason, initialization must occur with pwd being the julia
-      % bin directory
-      
-      % get mex function directory
-      s = functions(hdl);
-      bits = strsplit(s.file, filesep);
-      d = strjoin(bits(1:end-1), filesep);
-      
-      curdir = pwd();
-      cd(d);
-      
-      % initialize
-      hdl();
-      
-      cd(curdir);
     end
   end
 end
--- src/jl_call.cpp
+++ src/jl_call.cpp
@@ -2,9 +2,6 @@
 #include <julia.h>
 #include <vector>
 
-#define STR(s) #s
-#define XSTR(s) STR(s)
-
 void jl_check(const char *msg) {
   jl_value_t *e = jl_exception_occurred();
   if(e) {
@@ -17,45 +14,69 @@ void jl_check(const char *msg) {
   }
 }
 
-bool g_init = false;
+const size_t BUF_LEN = 1024;
+char g_julia_home[BUF_LEN];
+char g_julia_bin[BUF_LEN];
+char g_julia_image[BUF_LEN];
 
 void jl_finalize() {
   jl_atexit_hook(0);
 }
 
-void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
-  if (nrhs == 0) { // initilize, if necessary
+void mexFunction(int nl, mxArray* pl[], int nr, const mxArray* pr[]) {
 
-    if (!g_init) {
+  if (nr == 0) { // maybe dump some info, eventually
 
-      jl_init(XSTR(JULIA_INIT_DIR));
+  } else if (mxIsChar(pr[0])) { // call a function with this name
+
+    if(mxGetDimensions(pr[0])[0] == 0) { // empty string means initialization
+
+      if (jl_is_initialized()) return;
+
+      if(nr < 4 || !mxIsChar(pr[1]) || !mxIsChar(pr[2]) || !mxIsChar(pr[3]))
+        mexErrMsgTxt(
+          "Initialization requires 3 arguments:\n"
+          "\t1. The Julia lib directory;\n"
+          "\t2. The path to the Julia executable;\n"
+          "\t3. The path of the system image.");
+
+      mxGetString(pr[1], g_julia_home, BUF_LEN);
+      mxGetString(pr[2], g_julia_bin, BUF_LEN);
+      mxGetString(pr[3], g_julia_image, BUF_LEN);
+
+      libsupport_init();
+
+      jl_options.julia_home = g_julia_home;
+      jl_options.julia_bin = g_julia_bin;
+      jl_options.image_file = g_julia_image;
+
+      julia_init(JL_IMAGE_JULIA_HOME);
       jl_check("call to 'jl_init' failed");
+
       mexAtExit(jl_finalize);
 
-      g_init = true;
+      return;
     }
 
-  } else if (mxIsChar(prhs[0])) { // call a function with this name
-
     // get the function
-    char *fnName = mxArrayToString(prhs[0]);
+    char *fnName = mxArrayToString(pr[0]);
     jl_function_t *fn = jl_get_function(jl_main_module, fnName);
     mxFree(fnName);
     jl_check("call to 'jl_get_function' failed");
-    if(fn == NULL) mexErrMsgTxt("call to 'jl_get_function' returned NULL");
+    if (fn == NULL) mexErrMsgTxt("call to 'jl_get_function' returned NULL");
 
     jl_value_t *args[4];
-    args[0] = jl_box_int32(nlhs);
-    args[1] = jl_box_voidpointer(plhs);
-    args[2] = jl_box_int32(nrhs-1);
-    args[3] = jl_box_voidpointer(prhs+1);
+    args[0] = jl_box_int32(nl);
+    args[1] = jl_box_voidpointer(pl);
+    args[2] = jl_box_int32(nr-1);
+    args[3] = jl_box_voidpointer(pr+1);
     jl_call(fn, args, 4);
-    jl_check("call to 'jl_call' failed");
+    jl_exception_clear();
 
   } else { // evaluate the remaining arguments as strings
 
-    for(int i = 1; i < nrhs; ++i) {
-      char *expr = mxArrayToString(prhs[i]);
+    for (int i = 1; i < nr; ++i) {
+      char *expr = mxArrayToString(pr[i]);
       jl_eval_string(expr);
       mxFree(expr);
       jl_check("call to 'jl_eval_string' failed");
