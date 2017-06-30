ffc1bcbd17403fc5a1e0bdfa347a9c537b87a6f8
Tracy Wadleigh <tracy.wadleigh@gmail.com>
2017-06-30 12:39:19 -0700

    Fix lmdif example.

--- examples/lmdif.jl
+++ examples/lmdif.jl
@@ -1,6 +1,6 @@
-using Optim
+using LsqFit
 using Calculus
 
 # a simple wrapper for Levenberg-Marquardt with finite differencing for the
 # Jacobian
-lmdif(f, x0; show_trace::Bool=false) = Optim.levenberg_marquardt(f, jacobian(f), x0, show_trace=show_trace)
+lmdif(f, x0; show_trace::Bool=false) = LsqFit.levenberg_marquardt(f, jacobian(f), x0, show_trace=show_trace)
