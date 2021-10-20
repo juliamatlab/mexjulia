031a9b0de0c889e9e0ed52817775da6f321af0de
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2021-10-20 12:57:56 -0600

    update build file

--- deps/build.jl
+++ deps/build.jl
@@ -7,8 +7,6 @@ using MATLAB
 
 isDebug() = ccall(:jl_is_debugbuild, Cint, ()) != 0
 
-threadingOn() = ccall(:jl_threading_enabled, Cint, ()) != 0
-
 function shell_escape(str)
     str = replace(str, "'" => "'\''")
     return "\"$str\""
@@ -28,29 +26,20 @@ function includeDir()
     return abspath(Sys.BINDIR, Base.INCLUDEDIR, "julia")
 end
 
-
 function cflags()
-    flags = IOBuffer()
-    include = shell_escape(includeDir())
-    if isDebug()
-        print(flags, "-g")
-    else
-        print(flags, "-O")
+    fl = "-I $(shell_escape(includeDir()))"
+    if Sys.isunix()
+        fl = fl * " -fPIC"
     end
-    print(flags, " -I", include)
-    if threadingOn()
-        print(flags, " -DJULIA_ENABLE_THREADING")
-    end
-    return String(take!(flags))
+    return fl
 end
 
 function ldflags()
-    flags = IOBuffer()
-    print(flags, "-L$(shell_escape(libDir()))")
+    fl = "-L$(shell_escape(libDir()))"
     if Sys.isunix()
-        print(flags, " -Wl,-rpath $(shell_escape(libDir()))")
+        fl = fl * " -Wl,-rpath $(shell_escape(libDir()))"
     end
-    return String(take!(flags))
+    return fl
 end
 
 function ldlibs()
@@ -62,13 +51,12 @@ function ldlibs()
     if Sys.isunix()
         return "-l$libname -ldl"
     else
-        return realpath(joinpath(libDir(), "..", "lib", "lib$libname.dll.a"))
+        return "-l$libname -lopenlibm"
     end
 end
 
 # get build parameters
 is_debug = isDebug()
-threading_enabled = threadingOn()
 julia_bin = is_debug ?
     joinpath(unsafe_string(Base.JLOptions().julia_bindir), "julia-debug") :
     joinpath(unsafe_string(Base.JLOptions().julia_bindir), "julia")
@@ -76,20 +64,19 @@ julia_home = unsafe_string(Base.JLOptions().julia_bindir)
 sys_image = unsafe_string(Base.JLOptions().image_file)
 lib_base = is_debug ? "julia-debug" : "julia"
 lib_path = Libdl.dlpath("lib$lib_base")
-lib_dir = Sys.iswindows() ? joinpath(splitdir(julia_home)[1], "lib") : splitdir(lib_path)[1]
+lib_dir = Sys.iswindows() ? joinpath(dirname(julia_home), "lib") : dirname(lib_path)
 inc_dir = includeDir()
 build_cflags = cflags()
 build_ldflags = ldflags()
 build_ldlibs = ldlibs()
 build_src = abspath("mexjulia.cpp")
 outdir = joinpath(pwd(),"..","mexjulia")
-mex_cmd = "mex LDFLAGS=\'$(build_ldflags) \$LDFLAGS\' -v -largeArrayDims -outdir \"$outdir\" $(build_cflags) \"$(build_src)\" $(build_ldlibs)"
+mex_cmd = "mex -v -largeArrayDims -outdir \"$outdir\" LDFLAGS=\'$(build_ldflags) \$LDFLAGS\' CFLAGS=\'$(build_cflags) \$CFLAGS\' \"$(build_src)\" $(build_ldlibs)"
 
 # Save build parameters to .mat file
 mat"""
 % save build parameters to .mat file
 is_debug = $is_debug;
-threading_enabled = $threading_enabled;
 julia_bin = $julia_bin;
 julia_home = $julia_home;
 sys_image = $sys_image;
@@ -102,7 +89,7 @@ build_ldflags = $build_ldflags;
 build_ldlibs = $build_ldlibs;
 build_src = $build_src;
 mex_cmd = $mex_cmd;
-save("jldict.mat", "is_debug", "threading_enabled", "julia_bin", "julia_home",...
+save("jldict.mat", "is_debug", "julia_bin", "julia_home",...
     "sys_image", "lib_base", "lib_path", "lib_dir", "inc_dir", "build_cflags",...
     "build_ldflags", "build_ldlibs", "build_src", "mex_cmd");
 """
