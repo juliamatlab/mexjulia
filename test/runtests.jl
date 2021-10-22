0114ea318563643f1253c3ea37a81bd033685b2a
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2021-10-22 00:55:49 -0600

    suppress MATLAB output during tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,37 +1,42 @@
 using MATLAB, Test
 
+restart_default_msession(0)
+
 @testset "jl.eval" begin
+
     mat"""
-        $result = jl.eval('2+2')
+        $result = jl.eval('2+2');
     """
     @test result == 4
 
     mat"""
-        [$s, $c] = jl.eval('sin(pi/3), cos(pi/3)')
+        [$s, $c] = jl.eval('sin(pi/3), cos(pi/3)');
     """
     @test s ≈ sin(pi/3)
     @test c ≈ cos(pi/3)
 
     mat"""
-        jleval 1 + 1
-        $result = ans
+        jleval 1 + 1;
+        $result = ans;
     """
     @test result == 2
 end
 
 @testset "jl.call" begin
     mat"""
-        $result = jl.call('factorial', int64(10))
+        $result = jl.call('factorial', int64(10));
     """
     @test result == factorial(10)
 end
 
 @testset "jl.mex" begin
-    a = rand(5,5)
     mat"""
-    jleval import MATLAB
-    jleval double_it(args::Vector{MATLAB.MxArray}) = [2*MATLAB.jvalue(arg) for arg in args]
-    $result = jl.mex('double_it', $a)
+    a = rand(5,5);
+    jleval import MATLAB;
+    jleval double_it(args::Vector{MATLAB.MxArray}) = [2*MATLAB.jvalue(arg) for arg in args];
+    result = jl.mex('double_it', a);
+    $result = result{1};
+    $expected_result = 2*a;
     """
-    @test result ≈ 2*a
+    @test result ≈ expected_result
 end
