544e7165a3abc6cdb67a0e7d757299e21d5c0de5
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2021-10-22 12:25:11 -0600

    reorganize package

--- examples/interruption_test.m
+++ examples/interruption_test.m
@@ -3,14 +3,14 @@ function interruption_test()
 jleval println("Hit Ctrl-C to stop me!")
 
 % this one causes crashes (sometimes?)
-jleval 'x = 0; while(true); x += 1; if x%10000 == 0; println(x); end; yield(); end'
+jleval 'x = 0; while(true); global x += 1; if x%10000 == 0; println(x); end; yield(); end'
 
 
 % this one doesn't cause any problems
-% jleval 'x = 0; while(true); x += 1; yield(); end'
+% jleval 'x = 0; while(true); global x += 1; yield(); end'
 
 % this one isn't interruptable, as it never yields
-% jleval 'x = 0; while(true); x += 1; end'
+% jleval 'x = 0; while(true); global x += 1; end'
 
 
 end
