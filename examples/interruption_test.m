dfa1164ebf12bdb2c38eaef8627361ed7c8c713c
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2021-10-22 15:54:07 -0600

    remove extra space

--- examples/interruption_test.m
+++ examples/interruption_test.m
@@ -5,7 +5,6 @@ jleval println("Hit Ctrl-C to stop me!")
 % this one causes crashes (sometimes?)
 jleval 'global x = 0; while(true); global x += 1; if x%10000 == 0; println(x); end; yield(); end'
 
-
 % this one doesn't cause any problems
 % jleval 'global x = 0; while(true); global x += 1; yield(); end'
 
